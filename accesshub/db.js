4ea81c2ceb7d1b722f5a2a31dad999c109fb1e0f
syazaniyusra <syznyusra@gmail.com>
2026-05-13 08:11:56 +0800

    Update db.js

--- accesshub/db.js
+++ accesshub/db.js
@@ -1,15 +1,12 @@
 const mysql = require("mysql2/promise");
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-
   waitForConnections: true,
-  connectionLimit: 5,
+  connectionLimit: 4,  // ← change 5 to 4 (stay under MySQL's limit)
   queueLimit: 0
 });
-
 module.exports = pool;
